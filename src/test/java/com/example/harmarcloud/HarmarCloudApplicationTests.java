08e95b5efa604b91483c7c1b2230897ae3e5b89a
hemingyang_ClionGit <ttai33638@gamil.com>
2025-04-20 11:33:21 +0800

    Initial commit

---
+++ src/test/java/com/example/harmarcloud/HarmarCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.harmarcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmarCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
