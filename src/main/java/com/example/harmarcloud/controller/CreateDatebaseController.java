8f1cf217187791b9b60024fcb616e7423256bcd3
hemingyang_ClionGit <ttai33638@gamil.com>
2025-05-01 10:59:58 +0800

    Commit

--- src/main/java/com/example/harmarcloud/controller/CreateDatebaseController.java
+++ src/main/java/com/example/harmarcloud/controller/CreateDatebaseController.java
@@ -2,12 +2,15 @@ package com.example.harmarcloud.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CreateDatebaseController {
     @PostMapping("/datebase")
-    public ResponseEntity<String> createDatebase() {
+    public ResponseEntity<String> HandleCreateDatebase(
+            @RequestBody
+    ) {
 
     }
 }
